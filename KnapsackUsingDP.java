fad3eef7e37e81ee5b5bac31151bd174ca2ceb63
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-05-01 21:40:19 +0530

    knapSnack Using DP

---
+++ KnapsackUsingDP.java
@@ -0,0 +1,30 @@
+public class KnapsackUsingDP {
+
+    public static int knapsack(int[] weights, int[] values, int W) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] values = { 60, 100, 120 };
+        int[] weights = { 10, 20, 30 };
+        int W = 50;
+
+        int maxValue = knapsack(weights, values, W);
+        System.out.println("Maximum value that can be obtained: " + maxValue);
+    }
+}
